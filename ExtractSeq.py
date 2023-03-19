fa689983e362fcf0315c7991e1f9fccd0cbc5bef
tul10088 <114262010+tul10088@users.noreply.github.com>
2023-03-19 16:46:39 -0400

    Add files via upload

---
+++ ExtractSeq.py
@@ -0,0 +1,14 @@
+import timeit
+import pandas as pd
+
+# read the Excel file into a pandas dataframe
+df = pd.read_excel('/Users/aurelioaquila/Documents/America/TEMPLE/Research/uniprot.xlsx')
+
+# create a list of tuples from the dataframe, where each tuple represents a row of data
+data_list = [(row['Entry'], row['Protein names']) for index, row in df.iterrows()]
+
+# print the list
+print(data_list)
+
+startTime = timeit.default_timer()
+print("This algorithm takes: " + str(timeit.default_timer() - startTime) + " seconds")
