46065b69540ce7b2ce85171b6b5886bedd01f392
tul10088 <114262010+tul10088@users.noreply.github.com>
2023-03-19 18:50:09 -0400

    Update Benchmark.py

--- Benchmark.py
+++ Benchmark.py
@@ -5,39 +5,27 @@ from sklearn.cluster import OPTICS
 
 startTime = timeit.default_timer()
 matrixHammings = np.load("/Users/aurelioaquila/PycharmProjects/Practice/hammings1000.npy")
-print("1")
-# Define the distance metric
-def distance_metric(x, y):
-    return np.sqrt(np.sum((x-y)**2))
-print("2")
 
-# Create the reachability graph
-clustering = OPTICS(min_samples=10, max_eps=0.5, metric=distance_metric).fit(matrixHammings)
-print("3")
+# Fit the OPTICS algorithm to the data
+optics = OPTICS(min_samples=50, xi=0.05, min_cluster_size=0.1)
+optics.fit(matrixHammings)
 
-# Compute the ordering
-ordering = clustering.ordering_
-print("4")
+# Plot the clusters
+reachability = optics.reachability_
+labels = optics.labels_
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(set(labels)))]
 
-# Extract the clusters
-labels = clustering.labels_
-print("5")
+plt.figure(figsize=(10, 7))
+for klass, color in zip(range(0, len(colors)), colors):
+    Xk = matrixHammings[labels == klass]
+    reachabilityk = reachability[labels == klass]
+    plt.plot(Xk[:, 0], reachabilityk, 'o', markerfacecolor=tuple(color),
+             markeredgecolor='k', markersize=6)
 
-# Print the number of clusters and noise points
-num_clusters = len(set(labels)) - (1 if -1 in labels else 0)
-num_noise_points = list(labels).count(-1)
-print(f"Number of clusters: {num_clusters}")
-print(f"Number of noise points: {num_noise_points}")
-
-# Create a scatter plot of the data, colored by cluster
-plt.scatter(matrixHammings[:, 0], matrixHammings[:, 1], c=labels, cmap='viridis')
-
-# Add a colorbar to the plot
-plt.colorbar()
-
-# Add axis labels and a title to the plot
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.title('OPTICS Clustering')
+plt.title('OPTICS clustering')
+plt.xlabel('Data points')
+plt.ylabel('Reachability distance')
+plt.show()
 
 print("This algorithm takes: " + str(timeit.default_timer() - startTime) + " seconds")
